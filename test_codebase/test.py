243a7a983b8b7c1e7434501838607187c3e79ec0
K <theonetruekn@proton.me>
2024-05-03 16:06:53 +0200

    initial commit

---
+++ test_codebase/test.py
@@ -0,0 +1,13 @@
+class MyClass:
+    def __init__(self, one, two):
+        """
+        Init the class. Careful: do not touch!
+        """
+        self.one = one
+        self.two = two
+    
+    def do_stuff(self):
+        print("Do stuff")
+
+class MyClass2:
+    pass
