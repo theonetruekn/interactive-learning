9e6a8c177fe16eb3cd388d45976b0256058e6018
LuposX <schuppsimon5@gmail.com>
2024-06-09 23:02:11 +0200

    pytest

---
+++ tests/test_codebase/test.py
@@ -0,0 +1,19 @@
+"""
+Class Docstring
+"""
+class MyClass:
+   def __init__(self, one: str, two: int):
+        """
+        Init the class. Careful: do not touch!
+        """
+        self.one = one
+        self.two = two
+
+    def do_stuff(self) -> str:
+        """
+        This method does stuff.
+        """
+        print('test')
+
+class MyClass2:
+    pass
---
+++ tests/unittests/test_get_class_summary.py
@@ -0,0 +1,34 @@
+import os
+import pytest
+import importlib.util
+import inspect
+from unittest.mock import patch, mock_open, MagicMock
+
+from pathlib import Path 
+import sys 
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+
+import SmolCoder.src.tools.get_class_summary as im_class_summary
+
+def test_get_class_summary():
+    tool = im_class_summary.GetClassSummary()
+    root = os.path.join(os.path.dirname(__file__), '../test_codebase')
+
+    # Call the __call__ method
+    result = tool("MyClass", root)
+
+    expected_output = (
+        "def __init__(self, one: str, two: int):\n"
+        "    \"\"\"\n"
+        "    Init the class. Careful: do not touch!\n"
+        "    \"\"\"\n\n"
+        "def do_stuff(self) -> str:\n"
+        "    \"\"\"\n"
+        "    This method does stuff.\n"
+        "    \"\"\""
+    )
+    assert result == expected_output
+
+if __name__ == "__main__":
+    pytest.main()
