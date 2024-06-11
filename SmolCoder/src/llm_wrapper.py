031a74a1660b1e75a7738b1956d4874535d7338a
Konstantin <theonetruekn@proton.me>
2024-06-11 16:33:29 +0200

    makes querying chunkwise

--- SmolCoder/src/llm_wrapper.py
+++ SmolCoder/src/llm_wrapper.py
@@ -6,18 +6,55 @@ class LLM:
         self.model = model
         self.url = url
 
-    def query_completions(self, prompt, stream=False, options=None):
-            data = {
-                "model": self.model,
-                "prompt": prompt,
-                "stream": stream
-            }
+    # check out `raw` parameter too
+    def query_completion(self, prompt:str, stop_token="Observation") -> str:
+        data = {
+            "model": self.model,
+            "prompt": prompt,
+            "stream": True
+        }
 
-            if options is not None:
-                data['options'] = options
+        json_data = json.dumps(data)
+        response = requests.post(self.url, data=json_data, headers={'Content-Type': 'application/json'}, stream=True)
 
-            json_data = json.dumps(data)
+        partial_response = ""
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                chunk_data = json.loads(chunk.decode('utf-8'))
+                if "response" in chunk_data:
+                    partial_response += chunk_data["response"]
+                    if stop_token in partial_response:
+                        break
+                if chunk_data.get("done"):
+                    break
+        
+        if partial_response == "":
+            raise ValueError
 
-            response = requests.post(self.url, data=json_data, headers={'Content-Type': 'application/json'})
+        return partial_response
 
-            return response.json()["response"]
+if __name__ == "__main__":
+    llm = LLM("phi3:latest")
+    prompt = """You will be given `question` and you will respond with `answer`.
+
+To do this, you will interleave Thought, Action, and Observation steps.
+
+Thought can reason about the current situation, and Action can be the following types:
+(1) CodeSearch[search_query], which searches the codebase for a class or a function, specified as search_query in CodeSearch[search_query]. Returns the source code.. Example use: CodeSearch[my_func(param1, param2)] for searching a function or CodeSearch[SomeClass()] for searching a class.
+(2) Finish[answer], which returns the final `answer` and finishes the task. After calling this tool, you can stop generating.. Example use: Finish[The Answer is 42].
+---
+
+Follow the following format:
+
+Thought 1: Reasoning which action to take to solve the task.
+Action 1: Always either CodeSearch[search_query] or Finish[answer]
+Observation 1: result of Action 1
+Thought 2: next steps to take based on the previous Observation
+...
+until Action is of type Finish.
+
+---
+
+Question:  Which are the methods in MyClass?"""
+    response = llm.query_completion(prompt)
+    print(response)
