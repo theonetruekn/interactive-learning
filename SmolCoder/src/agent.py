809b00dea02c833ce89a3a17021517a0398a65d3
Konstantin <kayn@tutamail.com>
2024-06-17 18:33:58 +0200

    adds history

--- SmolCoder/src/agent.py
+++ SmolCoder/src/agent.py
@@ -2,6 +2,7 @@ import re
 
 from enum import Enum
 from pathlib import Path
+from typing import Optional
 
 from SmolCoder.src.prompting_strategy import PromptingStrategy
 from SmolCoder.src.aci import AgentComputerInterface
@@ -23,13 +24,20 @@ class SmolCoder:
     def __init__(self, model:LLM, codebase_dir:Path, toolkit:Toolkit, prompting_strategy:str = "ReAct") -> None:
         self.ACI = AgentComputerInterface(cwd=codebase_dir, tools=toolkit)
         self.prompting_strategy = PromptingStrategy.create(model, strategy=prompting_strategy, toolkit=toolkit)
+        self._history = []
     
     def _get_last_action(self, trajectory: str) -> str:
         matches = re.findall(r"Action:.*", trajectory)
         if matches:
             return matches[-1]
         else:
-            raise ValueError("No lines found")
+            raise ValueError("Couldn't extract an Action")
+    
+    def inspect_history(self, n:Optional[int] = None) -> str:
+        if not n:
+            return str(self._history)
+        else:
+            return str(self._history[-n:])
 
     def __call__(self, userprompt: str, max_calls:int = 10) -> str:
         trajectory = ""
@@ -39,9 +47,13 @@ class SmolCoder:
             else:
                 trajectory = self.prompting_strategy(prompt=trajectory, begin=False)
 
+            self._history.append(trajectory)
+
             action_sequence = self._get_last_action(trajectory)
             obs = self.ACI.get_observation(action_sequence)
             trajectory += obs
+
+            self._history.append(trajectory)
 
             if self.ACI.finished:
                 break
