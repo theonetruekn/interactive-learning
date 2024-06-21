cbdd6373ca122042aae0a4be2b155414947548ba
Konstantin <kayn@tutamail.com>
2024-06-21 16:55:53 +0200

    fixes most of the problems

--- SmolCoder/src/tools/execute_python.py
+++ SmolCoder/src/tools/execute_python.py
@@ -1,17 +1,14 @@
-#TODO: Test
-from typing import Any, List
-import os 
+import io
+import sys
+from typing import List
 
 from SmolCoder.src.tools.tool import Tool
 
 class ExecutePythonCode(Tool):
-    def __init__(self, path_to_file: str, filename: str):
-        self.path_to_file = path_to_file
-        self.filename = filename
 
     @property
     def name(self) -> str:
-        return "Execute_Python_Code"
+        return "Execute_Code"
 
     @property
     def input_variables(self) -> List[str]:
@@ -19,29 +16,30 @@ class ExecutePythonCode(Tool):
 
     @property
     def desc(self) -> str:
-        return "Executes the given python code"
+        return "executes the given python code and returns the value that is assigned to __result__"
 
     @property
     def example(self) -> str:
         raise NotImplementedError
 
-    def __call__(self, python_code: str) -> Any:
-        """"
-        Executes a string of Python code.
-
-        Args:
-            code_str: The string of Python code to execute.
-
-        Returns:
-            The output of the executed code, or None if there is no output.
-        """
-        with open(os.path.join(self.path_to_file, self.filename), "w+") as f:
-            f.write(python_code)
-
+    def __call__(self, input_variables: List[str]) -> str:
+        python_code = input_variables[0]
+        
         # Create a new namespace for the code to be executed in
         exec_namespace = {}
-        exec(python_code, exec_namespace)
-        # Get the result (if any) from the namespace
-        result = exec_namespace.get("__result__", None)
 
-        return result
+        # Capture the standard output
+        old_stdout = sys.stdout
+        new_stdout = io.StringIO()
+        sys.stdout = new_stdout
+
+        try:
+            exec(python_code, exec_namespace)
+        except Exception as e:
+            output = str(e)
+        else:
+            output = new_stdout.getvalue()
+        finally:
+            sys.stdout = old_stdout
+
+        return output if output else "No output found"
