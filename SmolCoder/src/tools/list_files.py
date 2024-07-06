61c393d1923875d3684216e4b311dd97b3615603
LuposX <schuppsimon5@gmail.com>
2024-07-07 00:44:15 +0200

    list_files only shows files in current directory

--- SmolCoder/src/tools/list_files.py
+++ SmolCoder/src/tools/list_files.py
@@ -14,27 +14,21 @@ class ListFiles(Tool):
 
     @property
     def desc(self) -> str:
-        return "lists all the files and subfolder that are in the folder."
+        return "lists all the files and subfolder that are in the current folder."
     
     @property 
     def example(self):
-        return f"{self.name}[some_dir] or {self.name}[.] to list the files of the current directory"
+        return f"{self.name}[.] to list the files of the current directory"
+
     #TODO: this should also work with "."
     def __call__(self, input_variables: List[str], cwd: Path, logger) -> str:
-        try:
-            folder_path = Path(input_variables[0])
-            full_path = Path(os.path.join(cwd, folder_path))
-            
-            if logger is not None: 
-                logger.debug("ListFiles with the path in: %s", full_path)
-        except Exception as e:
-            return "Something went wrong when parsing the path to the folder location: " + str(e)
-
+        full_path = cwd
+        
         if not full_path.exists():
-            return f'The specified folder does not exist: {folder_path}'
+            return f'The specified folder does not exist: {full_path}'
         
         if not full_path.is_dir():
-            return f'The specified path is not a folder: {folder_path}'
+            return f'The specified path is not a folder: {full_path}'
         
         try:
             entries = []
@@ -43,6 +37,6 @@ class ListFiles(Tool):
                 entries.append(f"{entry.name}{entry_suffix}")
             return f"The entries in `{str(full_path)}` are:\n" + "\n".join(entries)
         except PermissionError:
-            return f'Permission denied: Unable to access the folder: {folder_path}'
+            return f'Permission denied: Unable to access the folder: {full_path}'
         except Exception as e:
             return f'An error occurred: {e}'
